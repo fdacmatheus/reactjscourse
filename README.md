b67cb3b41358076d95a434f7bb74acc0736fbbaf
Matheus Ferreira <matheus.fdac@gmail.com>
2021-08-10 09:32:55 -0300

    Adicionado readmd e todos os arquivos

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto em react
+
+Projeto feito em react para aprender sobre a linguagem.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <a href="https://google.com.br">Dominick Lima mama muita pica</a>;
+};
+
+export default App;
